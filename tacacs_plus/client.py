b77d042c1a9961d9bd212553d2f777637d808a07
Karan Bazaz <kbazaz@cisco.com>
2018-02-26 17:17:55 -0800

    Added IPv6 support for tacacs_plus library

--- tacacs_plus/client.py
+++ tacacs_plus/client.py
@@ -32,7 +32,7 @@ class TACACSClient(object):
     http://www.shrubbery.net/tac_plus/
     """
 
-    def __init__(self, host, port, secret, timeout=10, session_id=None,
+    def __init__(self, host, port, secret, timeout=10, session_id=None,client_type="IPv4",
                  version_max=TAC_PLUS_MAJOR_VER,
                  version_min=TAC_PLUS_MINOR_VER):
         """
@@ -52,6 +52,7 @@ class TACACSClient(object):
         self.timeout = timeout
         self.version_max = version_max
         self.version_min = version_min
+        self.client_type = client_type
 
         # session_id is an unsigned 32-bit int; unless it's provided, randomize
         self.session_id = session_id or random.randint(1, 2 ** 32 - 1)
@@ -63,8 +64,13 @@ class TACACSClient(object):
     @property
     def sock(self):
         if not self._sock:  # pragma: nocover
-            conn = (self.host, self.port)
-            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if self.client_type == "IPv6":
+                #For AF_INET6 address family, a four-tuple (host, port, flowinfo, scopeid) is used
+                self._sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+                conn = (self.host, self.port,0,0)
+            else:
+                self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                conn = (self.host, self.port)
             self._sock.setblocking(1)
             self._sock.settimeout(self.timeout)
             self._sock.connect(conn)
